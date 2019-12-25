48235deb6b703bc8adbf10766aba7f4801cff5fa
OndrikB <38321117+OndrikB@users.noreply.github.com>
2019-12-25 13:55:35 +0100

    Fixed typos, added comments

--- initiator_set/gc_count/gc_count.py
+++ initiator_set/gc_count/gc_count.py
@@ -1,42 +1,48 @@
+#counts Gs and Cs in sequence
 def countG(mrna):
     return mrna.count(3)
 def countC(mrna):
     return mrna.count(2)
 
+#returns if a specific index is a G or C
 def isg(ind):
     return ind == 3
 def isc(ind):
     return ind == 2
 
-def magiccalculator(curscore,distance,istarget):
+#something for score calculation
+def magiccalculator(distance,istarget):
     if istarget:
-        return curscore + 5/distance
-    else
-        return curscore - 1/distance
+        return 5/distance
+    else:
+        return -1/distance
 
+#gets concentration scores for G and C
 def gscore(mrna,index):
     gscore = isg(mrna[index])*5
     for i in range(len(mrna)):
         if i-index != 0:
-            gscore = magiccalculator(gscore,abs(i-index),isg(mrna[i]))
+            gscore += magiccalculator(abs(i-index),isg(mrna[i]))
     return gscore
-
 def cscore(mrna,index):
     cscore = isc(mrna[index])*5
     for i in range(len(mrna)):
         if i-index != 0:
-            cscore = magiccalculator(cscore,abs(i-index),isc(mrna[i]))
+            cscore += magiccalculator(abs(i-index),isc(mrna[i]))
     return cscore
 
-
-def addresses(scores,treshold):
+#gets the points at which the concentrations are above a specific threshold
+def addresses(scores,threshold):
     points = []
     for i in range(len(scores)):
-        if scores[i] >= treshold:
+        if scores[i] >= threshold:
             points.append(i)
     return points
 
+
+#main function
 mrna = [] #temporary
+score_threshold = 0 #temporary
 gCount = countG(mrna)
 cCount = countC(mrna)
 gscores = []
@@ -44,6 +50,5 @@ cscores = []
 for i in range(len(mrna)):
     gscores.append(gscore(mrna,i))
     cscores.append(cscore(mrna,i))
-score_treshold = 0 #temporary
-highest_g = addresses(gscores,score_treshold)
-highest_c = addresses(cscores,score_treshold)
+highest_g = addresses(gscores,score_threshold)
+highest_c = addresses(cscores,score_threshold)
