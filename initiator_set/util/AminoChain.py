75a67bd09ce123b631b474180a7c766fb20dc0bb
foul-fortune-feline <foul-fortune-feline@pm.me>
2020-01-03 11:10:45 -0800

    Revert "All tabs to spaces, removed __pycache__ from fasta submodule"
    Fuck me, idiot I am working in the wrong brash and `&& git push`-ing
    
    This reverts commit 6586c5b796df84a1640df56b343e1d8d6c1f85ae.

--- initiator_set/util/AminoChain.py
+++ initiator_set/util/AminoChain.py
@@ -14,42 +14,42 @@ aminoCodons = "KNKNIIMIRSRSTTTT*Y*YLFLF*CWCSSSSEDEDVVVVGGGGAAAAQHQHLLLLRRRRPPPP"
 
 # Take a coding sequence and return the Amino Acid Chain it will translate into
 def convertRNA(seq:list, stcodon =0) -> list:
-  result:list = []
-  for i in range(0, len(seq), 3):
-    amino = aminoCodons[seq[i]]
-    if (amino == '*')
-      break
-    result.append(amino)
-  return result
+	result:list = []
+	for i in range(0, len(seq), 3):
+		amino = aminoCodons[seq[i]]
+		if (amino == '*')
+			break
+		result.append(amino)
+	return result
 
 
 
 
 class AminoAcidChain:
-  # Instantiates a new AminoAcidChain object using plain text
-  def __init__(self, seq:string):
-    self.sequence:string = seq
-    self.length:int = len(seq)
+	# Instantiates a new AminoAcidChain object using plain text
+	def __init__(self, seq:string):
+		self.sequence:string = seq
+		self.length:int = len(seq)
 
-    self.metadata:list = []
-
-
-  # Takes
-  # an mRNA object, a string of RNA in plain text, or an RNA index list
-  # and
-  # the codon index to begin reading from
-  # and returns a new AminoAcidChain
-  @classmethod
-  def fromCodingSequence(cls, seq, stcodon =0):
-    if isinstance(seq, mRNA.mRNA):
-      seq = seq.code
-    if isinstance(seq, str):
-      seq = indexRNA(seq)
-    if isinstance(seq, list):
-      return cls(convertRNA(seq, stcodon))
-    else:
-      raise TypeError("'seq' must be of type 'mRNA', 'str', or 'list'")
+		self.metadata:list = []
 
 
-  def __str__(self) -> str:
-    return self.sequence + "\n\n" + self.metadata
+	# Takes
+	# an mRNA object, a string of RNA in plain text, or an RNA index list
+	# and
+	# the codon index to begin reading from
+	# and returns a new AminoAcidChain
+	@classmethod
+	def fromCodingSequence(cls, seq, stcodon =0):
+		if isinstance(seq, mRNA.mRNA):
+			seq = seq.code
+		if isinstance(seq, str):
+			seq = indexRNA(seq)
+		if isinstance(seq, list):
+			return cls(convertRNA(seq, stcodon))
+		else:
+			raise TypeError("'seq' must be of type 'mRNA', 'str', or 'list'")
+
+
+	def __str__(self) -> str:
+		return self.sequence + "\n\n" + self.metadata
--- initiator_set/util/mRNA.py
+++ initiator_set/util/mRNA.py
@@ -12,83 +12,83 @@ bases = ['A', 'U', 'G', 'C'];
 
 
 def deindexCodon(index:int) -> str:
-  result = bases[int(index/16)]
-  result += bases[int((index/4))%4]
-  result += bases[index%4]
-  return result
+	result = bases[int(index/16)]
+	result += bases[int((index/4))%4]
+	result += bases[index%4]
+	return result
 
 
 
 def deindexRNA(code:list) -> str:
-  result = ''
-  for i in range (0, len(code), 3):
-    result = result + codonIndex(code[i])
+	result = ''
+	for i in range (0, len(code), 3):
+		result = result + codonIndex(code[i])
 
-  i = len(code) % 3
-  result = result[:-i] + codonIndex(code[-1])
-  return result
+	i = len(code) % 3
+	result = result[:-i] + codonIndex(code[-1])
+	return result
 
 
 
 def indexCodon(codon:str) -> int:
-  result = bases.index(codon[0]) * 16
-  result += bases.index(codon[1]) * 4
-  result += bases.index(codon[2])
-  return result
+	result = bases.index(codon[0]) * 16
+	result += bases.index(codon[1]) * 4
+	result += bases.index(codon[2])
+	return result
 
 
 
 def indexRNA(code:str) -> list:
-  result = []
-  for i in range(len(code) - 2):
-    result.append(indexCodon(code[i:i+3]))
-  return result
+	result = []
+	for i in range(len(code) - 2):
+		result.append(indexCodon(code[i:i+3]))
+	return result
 
 
 
 class mRNA:
 
 
-  def __init__(self, cd):
-    if isinstance(cd, str):
-      cd = indexRNA(cd)
-    if not isinstance(cd, list):
-      raise TypeError("'cd' must be of type 'str' or 'list'")
+	def __init__(self, cd):
+		if isinstance(cd, str):
+			cd = indexRNA(cd)
+		if not isinstance(cd, list):
+			raise TypeError("'cd' must be of type 'str' or 'list'")
 
-    self.code:list = cd
+		self.code:list = cd
 
-    # DEPRECATED! USE METADATA DICT INSTEAD!!!!!
-    self.baseWeights:list = []
-    self.adjustedWeights:list = []
-    # ^^^^^^^^^^^^ DEPRECATED ^^^^^^^^^^^^^^^^^^
+		# DEPRECATED! USE METADATA DICT INSTEAD!!!!!
+		self.baseWeights:list = []
+		self.adjustedWeights:list = []
+		# ^^^^^^^^^^^^ DEPRECATED ^^^^^^^^^^^^^^^^^^
 
-    self.metadata:dictionary = {}
+		self.metadata:dictionary = {}
 
 
 
-  def __str__(self) -> str:
-    return rnaIndex(self.code) + "\n\n" + self.metadata
+	def __str__(self) -> str:
+		return rnaIndex(self.code) + "\n\n" + self.metadata
 
 class Iterable(mRNA) :
 
-  def __init__(self, parent:mRNA, start:int, stop:int):
+	def __init__(self, parent:mRNA, start:int, stop:int):
 
-    self.parent:mRNA = parent
-    self.loc:int = start - 3
-    if stop <= start or stop > len(parent.code):
-      self.stop = len(parent.code)
-    else:
-      self.stop:int = stop
-
-
-  def __iter__(self):
-    return self
+		self.parent:mRNA = parent
+		self.loc:int = start - 3
+		if stop <= start or stop > len(parent.code):
+			self.stop = len(parent.code)
+		else:
+			self.stop:int = stop
 
 
-  def next(self):
-    self.loc += 3
-    if self.loc >= self.stop:
-      raise StopIteration
+	def __iter__(self):
+		return self
 
-    value = self.parent.code[self.loc:(self.loc + 3)]
-    return value
+
+	def next(self):
+		self.loc += 3
+		if self.loc >= self.stop:
+			raise StopIteration
+
+		value = self.parent.code[self.loc:(self.loc + 3)]
+		return value
