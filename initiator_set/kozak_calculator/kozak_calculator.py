955ce84ad3f986c35aab20f41c33ce7aa24424d5
Akasha Peppermint <tehrandommagik@gmail.com>
2020-02-07 20:10:26 +1000

    Feature addon to kozak calculator for last commit

--- initiator_set/kozak_calculator/kozak_calculator.py
+++ initiator_set/kozak_calculator/kozak_calculator.py
@@ -25,8 +25,10 @@ def calculate_kozaks(mrna: mRNA, kozaks_or_kozak_file: Union[List[KzConsensus], 
             kozaks = interpret_kozak_file(kz_raw, kozaks_or_kozak_file)
 
     ranked_weights = mrna.metadata["ranked_weights"]
+    locations_aic_chronological = mrna.metadata["locations_aic_chronological"]
     if ranked_weights is None:
         raise ValueError("Map AIC not initialised")
+
     kozaks_of_ranked_weights: List[float] = []
 
     kozak_start_codons = []
@@ -65,3 +67,40 @@ def calculate_kozaks(mrna: mRNA, kozaks_or_kozak_file: Union[List[KzConsensus], 
             kozaks_of_ranked_weights.append(0)
 
     mrna.metadata["kozaks_of_ranked_weights"] = kozaks_of_ranked_weights
+
+    ## TODO duplicated code
+
+    kozaks_of_chronological_weights: List[float] = []
+
+    # Attach kozak strength to each ranked weight
+    for start_index in locations_aic_chronological:
+        start_codon = deindexCodon(mrna.code[start_index])
+
+        try:
+            kz_i = kozak_start_codons.index(start_codon.lower())
+            kz = kozaks[kz_i]
+
+            leading_length = kz.codonStart
+            context_start = start_index - leading_length
+            context_end = context_start + len(kz.sequence)
+            code = deindexRNA(mrna.code)
+
+            # For now, skip kozaks that are too large to fix in mrna
+            # This might be undesirable, but probably irrelevant considering:
+            # 1. hard to read start codons too close to 5'
+            # 2. start codons near 3' are probably useless as they have no information
+            # to make it work though you would just have to pad z's around the context area
+            if context_start < 0:
+                continue
+                # context_area = ("z" * abs(context_start)) + context_area
+            if context_end > len(code):
+                continue
+                # context_area = context_area + ("z" * (context_end-len(mrna.code)))
+
+            context_area = code[context_start:context_end].lower()
+            similarity = kz.similarity(context_area)
+            kozaks_of_chronological_weights.append(similarity)
+        except ValueError:
+            kozaks_of_chronological_weights.append(0)
+
+    mrna.metadata["kozaks_of_aic_chronological"] = kozaks_of_chronological_weights
