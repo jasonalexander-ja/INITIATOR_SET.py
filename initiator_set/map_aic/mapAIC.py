67a745dd49d96c2db2a547262ce69062247d066f
Akasha Peppermint <tehrandommagik@gmail.com>
2020-03-08 01:31:33 +1000

    - Removed Map AIC Alternative, no longer needed - rankWeights no longer needed (why are there two of those methods?)

--- initiator_set/map_aic/mapAIC.py
+++ initiator_set/map_aic/mapAIC.py
@@ -20,31 +20,13 @@ except OSError as e:
 	raise
 
 
-
-def rankWeight(list_, weight, index):
-	for i in range(0, len(list_)):
-		if weight > list_[1]:
-			list_.insert(i, (index, weight))
-	i.append((index, weight))
-
-
-
-def rankWeight(list_, weight, index):
-	for i in range(0, len(list_)):
-		if weight > list_[1]:
-			list_.insert(i, (index, weight))
-	i.append((index, weight))
-
-
 def mapAICs(rna:mRNA.mRNA) -> mRNA.mRNA:
 	rna.metadata["baseWeights"] = []
-	rna.metadata["baseWeightsOrdered"] = []
 
 	for i in rna.code:
 		# Go to the desired entry in datafile
 		m_weights.seek(i * 4)
 		weight = unpack('<f', m_weights.read(4))[0]
 		rna.metadata["baseWeights"].append(weight)
-		rankWeight(rna.metadata["baseWeightsOrdered"], weight, i)
 
 	return rna
